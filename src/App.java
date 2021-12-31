1126dab6c579c4bb0ff90659a10dd423e05736ba
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-31 17:30:36 -0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main( String args[] ) {
+        String message;
+
+        message = "Hola a todos";
+        System.out.println( message );
+
+        message = "Bye Bye";
+        System.out.println( message );
+    }
+};
